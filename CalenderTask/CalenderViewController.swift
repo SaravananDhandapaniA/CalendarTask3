b63846b03c95516ab02b012333681e1236d33059
SaravananDhandapaniA <saravanan@tringapps.com>
2022-06-10 13:14:12 +0530

    Create a navication bar with add and search button and create a Calendar VC

---
+++ CalenderTask/CalenderViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CalenderViewController.swift
+//  CalenderTask
+//
+//  Created by Tringapps on 09/06/22.
+//
+
+import UIKit
+
+class CalenderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
